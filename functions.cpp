04d164b6d4a1af0ecb0c1c0e37986b3a0149d370
Kas9anushka <kumarianushkasingh62999@gmail.com>
2025-07-13 06:27:26 +0530

    function updated

--- functions.cpp
+++ functions.cpp
@@ -11,8 +11,56 @@ using namespace std;
     }   
 
 
+// calculate sum of digit 
+int sum(int a){
+     int add=0;
+    while(a!=0){
+    int x=a%10;
+    add+=x;
+    a=a/10;
+
+    }
+    return add;
+    
+
+}
+
+
+// calculate bionomial coeffient for nCr given n and r
+
+int bio(int n, int r){
+    int x=fact(n);
+    int y=fact(r);
+    int z=fact(n-r);
+    int value=x/(y*z);
+
+    return value;
+}
+
+// check the number is prime or not
+
+void prime(int x){
+    int fact=0;
+    for(int i=2;i<=x/2;i++){
+        if(x%i==0){
+            fact++;
+        }
+
+    }
+    if(fact==0){
+        cout<<"number is prime"<<endl;
+    }else{
+        cout<<"number is not prime"<<endl;
+    }
+}
+
+
+
 int main(){
 
     cout<<"factorial of a number : "<<fact(6)<<endl;
+    cout<<"sum of digit : "<< sum(789)<<endl;
+    cout<<"bionomial coeffient of a number : "<<bio(3,2)<<endl;
+    prime(8); // don't cout the void function.
 return 0;
 }
